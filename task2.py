ab56af79b76655c37e760a02b10dbdca01795f35
Raj-Rabidas-IITR <rajrabidas001@gmail.com>
2023-12-20 00:25:18 +0530

    Initial changes

---
+++ task2.py
@@ -0,0 +1,27 @@
+#Merging two or more pdfs
+import PyPDF2 as p2
+#Just simpple modifification made here
+
+
+pdf_list=["Mentoring-patent.pdf",'909.pdf']
+
+writer=p2.PdfWriter()
+
+# Merging algorithm
+
+for file in pdf_list:
+    reader=p2.PdfReader(open(file,"rb"))
+    for page in reader.pages:
+        writer.add_page(page)
+
+
+
+
+#save the merged pdf document
+
+with open("merged_file.pdf","wb") as f:
+    writer.write(f)
+#close the pdf writer
+writer.close()
+
+
